a559cd518504225a892be56910e4bdc3cada9204
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2019-11-05 15:36:06 -0600

    added x button to listed items, oriented a bit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
   $('button').click(function(){
     var toAdd = $('input[name=ListItem]').val();
-      $('ol').append('<li>' + toAdd + '</li>');
+      $('ol').append('<li>' + '<button id="x" onclick=removeFun(); >X</button>' + toAdd + '</li>');
   });
 });
+
+$('.ol').on('click', 'button', function() {
+  $(this).parent().remove();
+})
